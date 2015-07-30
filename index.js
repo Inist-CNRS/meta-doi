094ec7ac33ad63f03eae73de44af7b5a0c9dfab5
pseudom <dominique.lechaudel@gmail.com>
2015-07-30 16:04:01 +0200

    Tests for DOI request and API request

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ exports.DOIquery = function (doi, callback) {
   // CrossRef https://doi.crossref.org/search/doi?pid=inis:inis708&format=unixsd&doi=10.1016/0735-6757(91)90169-K
   
   var url = 'https://doi.crossref.org/search/doi?pid=inis:inis708&format=unixsd&doi=' + encodeURIComponent(doi);
-  console.log(doi);
 
   request.get(url, function (err, res, body) {
     if (err) { return callback(err); }
@@ -33,8 +32,50 @@ exports.DOIquery = function (doi, callback) {
     var info;
 
     try {
-      console.log(parser.toJson(body));
+      //console.log(parser.toJson(body));
       info = JSON.parse(parser.toJson(body));
+    } catch(e) {
+      //console.log(body);
+      return callback(e);
+    }
+
+    // if an error is thown, the json should contain the status code and a detailed message
+    if (info.error) {
+      var error = new Error(info.error.msg || 'got an unknown error from the API');
+      error.code = info.error.code;
+      return callback(error) ;
+    }
+
+    callback(null , info);
+  });
+};
+
+/**
+ * Query Crossref and get results
+ * @param  {Object}   search   the actual query parameters
+ * @param  {Object}   doi : doi to search metadata for
+ * @param  {Function} callback(err, result)
+ */
+exports.APIquery = function (doi, callback) {
+
+  // query link
+  // CrossRef https://doi.crossref.org/search/doi?pid=inis:inis708&format=unixsd&doi=10.1016/0735-6757(91)90169-K
+  
+  var url = 'http://api.crossref.org/works/' + encodeURIComponent(doi);
+  //console.log(doi);
+
+  request.get(url, function (err, res, body) {
+    if (err) { return callback(err); }
+
+    if (res.statusCode !== 200) {
+      return callback(new Error('unexpected status code : ' + res.statusCode));
+    }
+
+    var info;
+
+    try {
+      //console.log(body);
+      info = JSON.parse(body);
     } catch(e) {
       console.log(body);
       return callback(e);
@@ -51,8 +92,28 @@ exports.DOIquery = function (doi, callback) {
   });
 };
 
-exports.getPublicationDate = function(crossref_result) {
+exports.APIgetPublicationDateYear = function(api_result) {
+  return(api_result.message.issued['date-parts'][0][0]);
+};
 
-  var publicationDate = exports.query(doi, callback).crossref_result.query_result.body;
-  callback(null , publicationDate);
+exports.APIgetPublicationTitle = function(api_result) {
+  return(api_result.message['container-title'][0]);
+};
+
+exports.DOIgetPublicationDateYear = function(doc) {
+  var publication_date = doc.crossref_result.query_result.body.query.doi_record.crossref.journal.journal_article.publication_date;
+  var publication_date_year;
+  //console.log(publication_date);
+  if (typeof publication_date === 'object') {
+    if (typeof publication_date[0] === 'object') {
+      publication_date_year = publication_date[0].year;
+    } else if (publication_date.year === undefined) {
+      publication_date_year = "unknown";
+    } else {
+      publication_date_year = publication_date.year;            
+    }
+  } else {
+    publication_date_year = "unknown";
+  }
+  return(publication_date_year);
 };
--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 
 var path    = require('path');
 var should  = require('should');
-var crossref = require('./index.js');
+var metaDOI = require('./index.js');
 var testSet = [
   {
     "platform" : "ScienceDirect",
@@ -18,12 +18,12 @@ var testSet = [
   {
     "platform" : "Wiley",
     "doi": "10.1111/issg.12022",
-    "year": "2015"
+    "year": "2013"
   },
   {
     "platform" : "Wiley",
     "doi": "10.1111/j.1945-5100.1992.tb00734.x",
-    "year": "2015"
+    "year": "1992"
   },
   {
     "platform" : "BMJ",
@@ -32,25 +32,42 @@ var testSet = [
   }
 ];
 
-testSet.forEach(function(testCase) {
-  console.log(testCase.platform);
-  checkDOI(testCase);
+describe('Crossref doi', function () {
+  testSet.forEach(function(testCase) {
+    //console.log(testCase.platform);
+    DOIcheck(testCase);
+    APIcheck(testCase);
+  });
 });
 
-function checkDOI(testCase) {
-  console.log(testCase);
-  describe('crossref doi', function () {
-      console.log(testCase);
-      it('should be correctly enriched (@01) with ' + testCase.platform, function (done) {
-        console.log(testCase);
-
-        crossref.DOIquery(testCase.doi, function (err, doc) {
-          console.log("OOOO => ", testCase);
+function DOIcheck(testCase) {
+  //console.log(testCase);
+  describe('DOI request ', function () {
+      it('should be correctly enriched (@01) for ' + testCase.platform, function (done) {
+        metaDOI.DOIquery(testCase.doi, function (err, doc) {
           if (err) {
             console.error(err);
             exit(1);
           }
-          should.equal(doc.crossref_result.query_result.body.query.doi_record.crossref.journal.journal_article.publication_date.year, testCase.year);
+          should.equal(metaDOI.DOIgetPublicationDateYear(doc), testCase.year);
+          done();
+        });
+      });
+  });
+}
+
+function APIcheck(testCase) {
+  //console.log(testCase);
+  describe('API request ', function () {
+      //console.log(testCase);
+      it('should be correctly enriched (@02) for ' + testCase.platform, function (done) {
+        metaDOI.APIquery(testCase.doi, function (err, doc) {
+          if (err) {
+            console.error(err);
+            exit(1);
+          }
+          console.log(metaDOI.APIgetPublicationTitle(doc));
+          should.equal(metaDOI.APIgetPublicationDateYear(doc), testCase.year);
           done();
         });
       });
